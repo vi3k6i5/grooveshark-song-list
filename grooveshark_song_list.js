d76a9b8e7d6bc8f23573dfdfd6c45d832b46e79a
vi3k6i5 <vikash.duliajan@gmail.com>
2013-12-21 01:11:31 +0530

    added js to download list of songs from grooveshark

---
+++ grooveshark_song_list.js
@@ -0,0 +1,49 @@
+(function () {
+    var u = {}, songs = [], scrolled = 0, songList = [], checkCount = 0, previousSongCount = 0,
+    addToSongs = function(title, artist, album){
+    	var id = title + artist + album;
+        if(!u[id]){
+        	var song = {"title": title,
+        				"artist": artist,
+        				"album": album}
+        	songs.push(song);
+        	u[id] = 1;
+        }
+    },
+
+    displaySongs = function() {
+    	for (var i = 0; i < songs.length; i++) {
+    	    var obj = songs[i];
+    	    songList.push(JSON.stringify(songs[i]));
+    	}
+    	console.log('{"Songs":[' + songList + ']}');
+    };
+
+    $("#page-wrapper").scroll(function(){
+    	$(".title-container").each(function () {
+            var title = $(this).children("span").text();
+            var artist = $(this).parent().children(".meta-inner").children(".artist").text();
+            var album = $(this).parent().children(".meta-inner").children(".artist").text();
+            addToSongs(title, artist, album);
+        });
+    });    
+
+
+    _scrolldelay = setInterval( function() {
+    	scrolled = scrolled + 300;
+    	$("#page-wrapper").scrollTop(scrolled);
+
+        if(songs.length > previousSongCount) {
+            previousSongCount = songs.length;
+            checkCount = 0;
+        }
+        else if(songs.length === previousSongCount && checkCount < 3) {
+            checkCount++;
+        }
+        else if(checkCount >= 3){
+            clearInterval(_scrolldelay);
+            displaySongs();
+        }
+    },1000);
+
+}());
